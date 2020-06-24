89a3c493b17c6184fbedb7a31de7c3820884ec80
Stéphane Péchard <s13d@protonmail.com>
2020-06-24 10:39:31 +0200

    Use dependencies extracted from real project

---
+++ app/src/main/java/com/daxium/dependencies/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.daxium.dependencies
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
